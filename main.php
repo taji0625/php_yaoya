34d2685c92ec0412d09690f27d95deac8e4b4fa5
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2021-02-18 11:28:01 +0900

    八百屋プログラムの完成(仮)

--- main.php
+++ main.php
@@ -13,8 +13,21 @@ foreach($products as $i => $product) {
   echo "${i}. ${product['name']}(${product['price']}円)\n";
 }
 
+echo "\n";
 echo "商品の番号を選択 > ";
+echo "\n";
 $selectProductNum = trim(fgets(STDIN));
 $chosen_product = $products[$selectProductNum - 1];
 
-var_dump($chosen_product);
+echo "${chosen_product['name']}ですね。何個買いますか？\n\n";
+echo "個数を入力 > \n";
+$quantity = trim(fgets(STDIN));
+$totalAmount = $chosen_product['price'] * $quantity;
+
+if($quantity >= 5) {
+  echo "５個以上なので10%割引となります。\n";
+  $totalAmount *= 0.9;
+} 
+echo "合計金額は${totalAmount}円です。\n";
+echo "お買い上げありがとうございました！\n";
+
