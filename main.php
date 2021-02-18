ecdbb4e91cf12de1fd2256b61883f3229e2474f7
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2021-02-18 16:22:28 +0900

    プログラムをメソッドにまとめた

--- main.php
+++ main.php
@@ -1,5 +1,60 @@
 <?php
 
+// 商品を表示する
+function dispProducts($products)
+{
+  echo "いらっしゃいませ！商品を選んで下さい。\n\n";
+  foreach($products as $i => $product) {
+    $i++;
+    echo "${i}. ${product['name']}(${product['price']}円)\n";
+  }
+}
+
+// 商品を選択
+function chooseProduct($products)
+{
+  echo "\n";
+  while(true) {
+    echo "商品の番号を選択 > ";
+    $selectProductNum = trim(fgets(STDIN));
+    if ($selectProductNum == 1 || $selectProductNum == 2 || $selectProductNum == 3 || $selectProductNum == 4) {
+      break;
+    }
+    echo "\n1~4の番号を入力して下さい！\n\n";
+  }
+  echo "\n";
+  $selectProductNumber = (int)$selectProductNum;
+  return $products[$selectProductNumber - 1];
+}
+
+// 商品の個数を決定
+function decideQuantity($chosenProduct)
+{
+  echo "${chosenProduct['name']}ですね。何個買いますか？\n\n";
+  while (true) {
+    echo "個数を入力 > ";
+    $quant = trim(fgets(STDIN));
+    if (is_numeric($quant)) {
+      break;
+    }
+    echo "個数を入力してください！\n\n";
+  }
+  $quantity = (int)$quant;
+  return $quantity;
+}
+
+// 合計金額を計算
+function calculateCharges($chosenProduct, $quantityOfProduct)
+{
+  $totalAmount = $chosenProduct["price"] * $quantityOfProduct;
+  if($quantityOfProduct >= 5) {
+    echo "\n5個以上なので10%割引となります。\n";
+    $totalAmount *= 0.9;
+  } 
+  echo "合計金額は${totalAmount}円です。\n";
+  echo "お買い上げありがとうございました！\n";
+}
+
 $products = [
   ['name' => 'トマト' , 'price' => 100],
   ['name' => 'きゅうり' , 'price' => 200],
@@ -7,40 +62,7 @@ $products = [
   ['name' => 'なす' , 'price' => 400],
 ];
 
-echo "いらっしゃいませ！商品を選んで下さい。\n\n";
-foreach($products as $i => $product) {
-  $i++;
-  echo "${i}. ${product['name']}(${product['price']}円)\n";
-}
-
-echo "\n";
-while(true) {
-  echo "商品の番号を選択 > ";
-  $selectProductNum = trim(fgets(STDIN));
-  if ($selectProductNum == 1 || $selectProductNum == 2 || $selectProductNum == 3 || $selectProductNum == 4) {
-    break;
-  }
-  echo "\n1~4の番号を入力して下さい！\n\n";
-}
-echo "\n";
-$chosen_product = $products[$selectProductNum - 1];
-
-echo "${chosen_product['name']}ですね。何個買いますか？\n\n";
-
-while (true) {
-  echo "個数を入力 > ";
-  $quantity = trim(fgets(STDIN));
-  if (is_numeric($quantity)) {
-    break;
-  }
-  echo "\n個数を入力して下さい！\n\n"; 
-}
-$totalAmount = $chosen_product['price'] * $quantity;
-
-if($quantity >= 5) {
-  echo "\n５個以上なので10%割引となります。\n";
-  $totalAmount *= 0.9;
-} 
-echo "合計金額は${totalAmount}円です。\n";
-echo "お買い上げありがとうございました！\n";
-
+dispProducts($products);
+$chosenProduct = chooseProduct($products);
+$quantityOfProduct = decideQuantity($chosenProduct);
+calculateCharges($chosenProduct, $quantityOfProduct );
