5f8a3e7da49ef56d54caabd7c6c0b0d9750f3d5f
Takuma Tajikawa <t.g.k.w.t.k.m@gmail.com>
2021-02-18 11:09:08 +0900

    商品番号の入力を実装

--- main.php
+++ main.php
@@ -9,5 +9,12 @@ $products = [
 
 echo "いらっしゃいませ！商品を選んで下さい。\n\n";
 foreach($products as $i => $product) {
+  $i++;
   echo "${i}. ${product['name']}(${product['price']}円)\n";
 }
+
+echo "商品の番号を選択 > ";
+$selectProductNum = trim(fgets(STDIN));
+$chosen_product = $products[$selectProductNum - 1];
+
+var_dump($chosen_product);
